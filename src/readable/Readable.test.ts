279b07e4962ef2234544932e51ef05fa53827df5
hvi <hvi@lesfurets.com>
2019-08-16 17:42:49 +0200

    Retrieve first implem from other repo

---
+++ src/readable/Readable.test.ts
@@ -0,0 +1,136 @@
+import { r } from "./Readable";
+
+describe("isTrue", () => {
+  it("should return true if is truthy", () => {
+    const result = r("truthy string")`my input`.isTrue();
+    expect(result.print()).toBe("my input is true");
+    expect(result.eval()).toBe(true);
+  });
+  it("should return false if is falsy", () => {
+    const result = r(undefined)`my input`.isTrue();
+    expect(result.print()).toBe("my input is not true");
+    expect(result.eval()).toBe(false);
+  });
+});
+
+describe("isFalse", () => {
+  it("should return true if is falsy", () => {
+    const result = r("")`my falsy input`.isFalse();
+    expect(result.print()).toBe("my falsy input is false");
+    expect(result.eval()).toBe(true);
+  });
+  it("should return false if is truthy", () => {
+    const result = r(true)`my truthy input`.isFalse();
+    expect(result.print()).toBe("my truthy input is not false");
+    expect(result.eval()).toBe(false);
+  });
+});
+
+describe("isEqualTo", () => {
+  it("should be true when values are equal", () => {
+    const result = r(true)``.isEqualTo(r(true)`something else`);
+    expect(result.print()).toBe("true is equal to something else");
+    expect(result.eval()).toBe(true);
+  });
+  it("should be false when values are not equal", () => {
+    const result = r(true)``.isEqualTo(r(false)`something else`);
+    expect(result.print()).toBe("true is not equal to something else");
+    expect(result.eval()).toBe(false);
+  });
+});
+
+describe("and", () => {
+  it("should be true when both are true", () => {
+    const result = r(true)``.and(r(true)``);
+    expect(result.print()).toBe("true and true");
+    expect(result.eval()).toBe(true);
+  });
+  it("should be false when one is true", () => {
+    const result = r(true)``.and(r(false)``);
+    expect(result.print()).toBe("true and false");
+    expect(result.eval()).toBe(false);
+  });
+});
+
+describe("or", () => {
+  it("should be true when on is true", () => {
+    const result = r(true)``.or(r(false)``);
+    expect(result.print()).toBe("true or false");
+    expect(result.eval()).toBe(true);
+  });
+  it("should be false when both are false", () => {
+    const result = r(false)``.or(r(false)``);
+    expect(result.print()).toBe("false or false");
+    expect(result.eval()).toBe(false);
+  });
+});
+
+describe("map", () => {
+  it("should print the function when not a const", () => {
+    const result = r(true)``.map(b => !b);
+    expect(result.print()).toBe("true mapped by b => !b");
+    expect(result.eval()).toBe(false);
+  });
+  it("should print the function name when named", () => {
+    const not = b => !b;
+    const result = r(true)``.map(not);
+    expect(result.print()).toBe("true mapped by not");
+    expect(result.eval()).toBe(false);
+  });
+  it("should apply the function to it", () => {
+    const not = b => !b;
+    const input = true;
+    const result = r(input)``.map(not);
+    expect(result.print()).toBe("true mapped by not");
+    expect(result.eval()).toBe(not(true));
+  });
+  it("should be distributive", () => {
+    const add1 = b => b + 1;
+    const toStringExcl = b => String(b) + "!!!";
+    const result1 = r(1)``.map(add1).map(toStringExcl);
+    const result2 = r(1)``.map(it => toStringExcl(add1(it)));
+    expect(result1.print()).toBe("1 mapped by add1 mapped by toStringExcl");
+    expect(result1.eval()).toBe("2!!!");
+    expect(result2.print()).toBe("1 mapped by it => toStringExcl(add1(it))");
+    expect(result2.eval()).toBe("2!!!");
+    expect(result2.eval()).toBe(result1.eval());
+  });
+});
+
+it("should apply", () => {
+  const result = r(true)``
+    .apply(r((b: boolean) => !b)`negation`)
+    .isEqualTo(r(false)``);
+  expect(result.print()).toBe("true mapped by negation is equal to false");
+  expect(result.eval()).toBe(true);
+});
+
+it("should apply and print function", () => {
+  const result = r(true)``.apply(r((b: boolean) => !b)``).isEqualTo(r(false)``);
+  expect(result.print()).toBe("true mapped by (b) => !b is equal to false");
+  expect(result.eval()).toBe(true);
+});
+
+it("should be false when isFalse", () => {
+  const result = r(true)``.isFalse();
+  expect(result.print()).toBe("true is not false");
+  expect(result.eval()).toBe(false);
+});
+
+it("should be true when isFalse", () => {
+  const result = r(false)``.isFalse();
+  expect(result.print()).toBe("false is false");
+  expect(result.eval()).toBe(true);
+});
+
+it("should access value with s", () => {
+  interface Person {
+    name: string;
+  }
+  const john: Person = {
+    name: "Doe"
+  };
+  const result = r(john)`John`.s("name");
+  expect(result.print()).toBe("John's name");
+  expect(result.eval()).toBe("Doe");
+});
---
+++ src/readable/Readable.ts
@@ -0,0 +1,77 @@
+class Readable<A> {
+  it: A;
+  itsName: string;
+
+  constructor(it: A, itsName: string) {
+    this.it = it;
+    this.itsName = itsName;
+  }
+
+  isTrue(): Readable<boolean> {
+    return this.it
+      ? new Readable(!!this.it, `${this.itsName} is true`)
+      : new Readable(!!this.it, `${this.itsName} is not true`);
+  }
+
+  isFalse(): Readable<boolean> {
+    return !this.it
+      ? new Readable(!this.it, `${this.itsName} is false`)
+      : new Readable(!this.it, `${this.itsName} is not false`);
+  }
+
+  isEqualTo(them: Readable<A>): Readable<boolean> {
+    const isEqual = this.it === them.it;
+    return new Readable(
+      isEqual,
+      `${this.itsName} is ${isEqual ? "" : "not "}equal to ${them.itsName}`
+    );
+  }
+
+  and<B>(them: Readable<B>): Readable<boolean> {
+    return new Readable(
+      !!this.it && !!them.it,
+      `${this.itsName} and ${them.itsName}`
+    );
+  }
+
+  or<B>(them: Readable<B>): Readable<boolean> {
+    return new Readable(
+      !!this.it || !!them.it,
+      `${this.itsName} or ${them.itsName}`
+    );
+  }
+
+  map<B>(fn: (it: A) => B): Readable<B> {
+    const fnName = fn.name ? fn.name : fn;
+    return new Readable(fn(this.it), `${this.itsName} mapped by ${fnName}`);
+  }
+
+  flatMap<B>(fn: (it: A) => Readable<B>): Readable<B> {
+    const result = fn(this.it);
+    return new Readable(
+      result.it,
+      `${this.itsName} mapped by ${result.itsName}`
+    );
+  }
+
+  apply<B>(them: Readable<(it: A) => B>): Readable<B> {
+    const result = them.it(this.it);
+    return new Readable(result, `${this.itsName} mapped by ${them.itsName}`);
+  }
+
+  s(key: keyof A): Readable<A[keyof A]> {
+    const value = this.it[key];
+    return new Readable(value, `${this.itsName}'s ${key}`);
+  }
+
+  eval(): A {
+    return this.it;
+  }
+
+  print(): string {
+    return this.itsName;
+  }
+}
+
+export const r = <A>(it: A) => (literals: TemplateStringsArray): Readable<A> =>
+  literals[0] ? new Readable<A>(it, literals[0]) : new Readable(it, String(it));
